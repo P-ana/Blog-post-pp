bb3f2c8e453b333b5bc590107f749d291c0901fd
P-ana <anarakic.a@gmail.com>
2025-03-19 13:39:27 +0100

    Completed index page

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render("index.ejs");});
+
+app.get('/form', (req, res) => {
+    res.render('form.ejs'); 
+    });
+    
+app.listen(port, () => {
+    console.log(`listening on ${port}`);});
